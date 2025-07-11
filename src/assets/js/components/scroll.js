1ceb8f69adabff9aa00e9fbca3cc7508ba202fb8
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2025-07-11 15:57:07 -0300

    Atualização dos arquivos HTML e JavaScript com remoção de metadados desnecessários, ajustes de formatação e melhorias na legibilidade. Alterações no index.html para simplificar a estrutura e otimizar a apresentação visual. Modificações no scroll.js para padronizar a formatação do código. Ajustes nas páginas portal.html e simulação.html para melhor organização das referências de CSS e inclusão de favicon.

--- src/assets/js/components/scroll.js
+++ src/assets/js/components/scroll.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const backToTopBtn = document.getElementById('backToTopBtn');
-    
-    window.addEventListener('scroll', function() {
+
+    window.addEventListener('scroll', function () {
         // Mostrar botão apenas após 50% da altura da página
         const halfPageHeight = document.documentElement.scrollHeight / 2;
         if (window.pageYOffset > halfPageHeight) {
@@ -10,9 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
             backToTopBtn.classList.remove('visible');
         }
     });
-    
+
     // Rolagem suave ao topo
-    backToTopBtn.addEventListener('click', function(e) {
+    backToTopBtn.addEventListener('click', function (e) {
         e.preventDefault();
         window.scrollTo({
             top: 0,
