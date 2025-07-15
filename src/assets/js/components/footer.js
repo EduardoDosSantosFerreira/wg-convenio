f4209ab3b277ab317fe772037f4b16b0c1c58065
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2025-07-15 08:38:00 -0300

    Remoção de links duplicados para o login do administrador no rodapé, simplificando a estrutura do código e melhorando a legibilidade.

--- src/assets/js/components/footer.js
+++ src/assets/js/components/footer.js
@@ -51,10 +51,7 @@ function adicionarLoginAoDOM() {
             <div class="col-md-6 text-md-end social-icons d-flex justify-content-md-end align-items-center gap-3">
                 <a href="https://www.facebook.com/wgconvenio/" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
                 <a href="https://www.instagram.com/wgconvenio/" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
-                <a href="../../../../admin/securaty.html" aria-label="Login do administrador">
-                    <i class="fas fa-user-circle text-light" style="font-size: 1.8rem;"></i>
-                </a>
-                    <a href="../../../../../wg-convenio/admin/securaty.html" aria-label="Login do administrador">
+                <a href="../../../../../wg-convenio/admin/securaty.html" aria-label="Login do administrador">
                     <i class="fas fa-user-circle text-light" style="font-size: 1.8rem;"></i>
                 </a>
             </div>
